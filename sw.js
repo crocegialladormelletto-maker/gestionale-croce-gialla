01d2a1ccdc5c438cef37cb4a71e15f6b537008b3
crocegialladormelletto-maker <crocegialladormelletto@gmail.com>
2026-09-25 12:41:26 +0200

    Forza aggiornamento cache APP Volontari V13.43

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Questa versione aggiorna la PWA esistente senza richiedere una nuova installazione.
-const CACHE_NAME = 'cge-volontari-static-v9';
+const CACHE_NAME = 'cge-volontari-static-v10';
 const APP_BASE = '/gestionale-croce-gialla/';
 const STATIC_ASSETS = new Set([
   APP_BASE + 'manifest.json',
