5520a2aa398d2d74c18da9cc01fc05099eb1f42b
crocegialladormelletto-maker <crocegialladormelletto@gmail.com>
2026-09-25 12:25:48 +0200

    Forza nuova cache PWA volontari v13.40

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Questa versione aggiorna la PWA esistente senza richiedere una nuova installazione.
-const CACHE_NAME = 'cge-volontari-static-v6';
+const CACHE_NAME = 'cge-volontari-static-v7';
 const APP_BASE = '/gestionale-croce-gialla/';
 const STATIC_ASSETS = new Set([
   APP_BASE + 'manifest.json',
