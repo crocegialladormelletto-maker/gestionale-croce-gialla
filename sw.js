805e63fcd8fb3a459f9aa4ed82f424dde925f772
crocegialladormelletto-maker <crocegialladormelletto@gmail.com>
2026-09-26 09:11:19 +0200

    Aggiorna cache dopo correzione versione V13.48

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Questa versione aggiorna la PWA esistente senza richiedere una nuova installazione.
-const CACHE_NAME = 'cge-volontari-static-v14';
+const CACHE_NAME = 'cge-volontari-static-v15';
 const APP_BASE = '/gestionale-croce-gialla/';
 const STATIC_ASSETS = new Set([
   APP_BASE + 'manifest.json',
